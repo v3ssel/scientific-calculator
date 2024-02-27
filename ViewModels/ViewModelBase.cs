27e31cfcaf9477c9e77f9ab8dd36de145a65e09a
v3ssel <v3ssels@pm.me>
2024-02-27 15:54:42 +0300

    avalonia template

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace scientific_calc.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace scientific_calc.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
