a8b54366678d900eb23ab5aca55c45c2255da7a3
v3ssel <v3ssels@pm.me>
2024-02-28 23:51:05 +0300

    clipboard experiments

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,101 @@
-﻿namespace ScientificCalculator.ViewModels;
+﻿using System;
+using System.Reactive;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.Input.Platform;
+using ReactiveUI;
+
+namespace ScientificCalculator.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    private string _expressionInput = "";
+    public string ExpressionInput
+    {
+        get => _expressionInput;
+        set => this.RaiseAndSetIfChanged(ref _expressionInput, value);
+    }
+
+    private int _expressionInputCaretIndex;
+
+    public int ExpressionInputCaretIndex
+    {
+        get => _expressionInputCaretIndex;
+        set => this.RaiseAndSetIfChanged(ref _expressionInputCaretIndex, value);
+    }
+    private int _expressionInputSelectionStart;
+
+    public int ExpressionInputSelectionStart
+    {
+        get => _expressionInputSelectionStart;
+        set => this.RaiseAndSetIfChanged(ref _expressionInputSelectionStart, value);
+    }
+    private int _expressionInputSelectionEnd;
+
+    public int ExpressionInputSelectionEnd
+    {
+        get => _expressionInputSelectionEnd;
+        set => this.RaiseAndSetIfChanged(ref _expressionInputSelectionEnd, value);
+    }
+
+    private string _answerField = "";
+    public string AnswerField
+    {
+        get => _answerField;
+        set => this.RaiseAndSetIfChanged(ref _answerField, value);
+    }
+
+    public void CalculateBtnAction()
+    {
+        AnswerField = ExpressionInput;
+    }
+
+    public void CleanInputAction()
+    {
+        ExpressionInput = "";
+    }
+
+    public ReactiveCommand<IClipboard, Unit> CopyInputActionCmd { get; }
+    public ReactiveCommand<IClipboard, Unit> PasteInputActionCmd { get; }
+
+    public MainWindowViewModel()
+    {
+        CopyInputActionCmd = ReactiveCommand.CreateFromTask<IClipboard>(CopyInputAction);
+        PasteInputActionCmd = ReactiveCommand.CreateFromTask<IClipboard>(PasteInputAction);
+    }
+
+    public async Task CopyInputAction(IClipboard clipboard)
+    {
+        if (ExpressionInputSelectionStart == ExpressionInputSelectionEnd)
+        {
+            await clipboard.SetTextAsync(ExpressionInput);
+        }
+        else
+        {
+            var start = Math.Min(ExpressionInputSelectionStart, ExpressionInputSelectionEnd);
+            var end = Math.Max(ExpressionInputSelectionStart, ExpressionInputSelectionEnd);
+
+            await clipboard.SetTextAsync(ExpressionInput[start..end]);
+        }
+    }
+    
+    public async Task PasteInputAction(IClipboard clipboard)
+    {
+        var clipboard_text = await clipboard.GetTextAsync() ?? "";
+        if (string.IsNullOrEmpty(clipboard_text)) return;
+        
+        if (ExpressionInputSelectionStart == ExpressionInputSelectionEnd)
+        {
+            ExpressionInput = ExpressionInput.Insert(ExpressionInputCaretIndex, clipboard_text);
+        }
+        else
+        {
+            var start = Math.Min(ExpressionInputSelectionStart, ExpressionInputSelectionEnd);
+            var end = Math.Max(ExpressionInputSelectionStart, ExpressionInputSelectionEnd);
+
+            ExpressionInput = ExpressionInput.Remove(start, end - start).Insert(start, clipboard_text);
+        }
+        
+        ExpressionInputCaretIndex += clipboard_text.Length;
+    }
 }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System;
+using System.Diagnostics;
 
 namespace ScientificCalculator.Views;
 
@@ -7,5 +10,29 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    public void ExpandButtonClicked(object source, RoutedEventArgs args)
+    {
+    //     Console.WriteLine("Click!");
+    //     Debug.WriteLine("Click!");
+    //     answer_textbox.Text = input_textbox.Text;
+        
+        // if (main_split.IsPaneOpen)
+        // {
+            // (source as TextBox).SelectedText
+        //     main_split.IsPaneOpen = false;
+        //     expand_btn.Content = "<";
+        //     expand_btn.HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center;
+        //     expand_btn.Margin = Avalonia.Thickness.Parse("0 10 0 0");
+        //     // (source as Button)?
+        // }
+        // else
+        // {
+        //     main_split.IsPaneOpen = true;
+        //     expand_btn.Content = ">";
+        //     expand_btn.HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Left;
+        //     expand_btn.Margin = Avalonia.Thickness.Parse("10 10 0 0");
+        // }
     }
 }
