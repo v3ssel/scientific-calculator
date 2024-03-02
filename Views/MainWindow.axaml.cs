6ea99bae60413c878ff76f1fc817758a7d0e1ff1
v3ssel <v3ssels@pm.me>
2024-03-02 22:39:45 +0300

    resize animation

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,11 +1,15 @@
+using Avalonia;
 using Avalonia.Animation;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
 using Avalonia.Styling;
+using ReactiveUI;
 using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ScientificCalculator.Views;
 
@@ -13,57 +17,54 @@ public partial class MainWindow : Window
 {
     public MainWindow()
     {
-        // this.Transitions = new Transitions();
         InitializeComponent();
 
-        // this.Transitions.Add(new DoubleTransition
-        // {
-        //     Property = Window.WidthProperty,
-        //     Duration = TimeSpan.FromMilliseconds(5000)
-        // });
+
     }
  
     public void PaneOpeningEvent(object source, CancelRoutedEventArgs args)
     {
         if (this.IsActive)
-        {   
-            var st = new Style(x => x.Is(typeof(Window)));
-
-            var anim = new Animation
-            {
-                Duration = TimeSpan.FromMilliseconds(500)
-            };
-
-            var start_kf = new KeyFrame
-            {
-                Cue = Cue.Parse("0%", System.Globalization.CultureInfo.InvariantCulture)
-            };
-            start_kf.Setters.Add(new Setter(MainWindow.WidthProperty, this.Width));
-
-            var end_kf = new KeyFrame
-            {
-                Cue = Cue.Parse("100%", System.Globalization.CultureInfo.InvariantCulture)
-            };
-            end_kf.Setters.Add(new Setter(MainWindow.WidthProperty, this.Width += main_split.OpenPaneLength - main_split.CompactPaneLength));
-
-            anim.Children.Add(start_kf);
-            anim.Children.Add(end_kf);
-            st.Animations.Add(anim);
-
-            this.Styles.Add(st);
-
-            this.Width += main_split.OpenPaneLength - main_split.CompactPaneLength;
+        {
+            StartAnimation(MainWindow.WidthProperty, this.Width, this.Width + (main_split.OpenPaneLength - main_split.CompactPaneLength));
         }
     }
 
     public void PaneClosingEvent(object source, CancelRoutedEventArgs args)
     {
         if (this.IsActive)
-            this.Width -= main_split.OpenPaneLength - main_split.CompactPaneLength;
+        {
+            StartAnimation(MainWindow.WidthProperty, this.Width, this.Width - (main_split.OpenPaneLength - main_split.CompactPaneLength));
+        }
     }
 
     public void ExpandButtonClicked(object source, RoutedEventArgs args)
     {   
         // (source as TextBox).SelectedText
+    }
+
+    private void StartAnimation(AvaloniaProperty property, object initial_value, object desired_value)
+    {
+        var anim = new Animation
+        {
+            Duration = TimeSpan.FromMilliseconds(50)
+        };
+
+        var start_kf = new KeyFrame
+        {
+            Cue = new Cue(0)
+        };
+        start_kf.Setters.Add(new Setter(property, initial_value));
+
+        var end_kf = new KeyFrame
+        {
+            Cue = new Cue(1)
+        };
+        end_kf.Setters.Add(new Setter(property, desired_value));
+
+        anim.Children.Add(start_kf);
+        anim.Children.Add(end_kf);
+
+        anim.RunAsync(this);
     }
 }
